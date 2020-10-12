ce619163463a5036ddcc5cfe1b6bb397def3475e
Zachary Perkins <ezmac028@gmail.com>
2020-10-12 17:15:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ function nowServing(katzDeliLine){
   if (katzDeliLine.length > 0) return `Currently serving ${katzDeliLine.shift()}.`;
   else return "There is nobody waiting to be served!";
 }
+
+function currentLine(katzDeliLine){
+  var str = `The line is currently: `;
+  var newLine = [];
+  if (katzDeliLine.length === 0) return "the line is currently empty.";
+  else{
+    for (let i = 0; i < katzDeliLine.length; i++){
+      newLine[i] = `${[i+1]}. $katzDeliLine[i]}`;
+    }
+    str  += newLine.join(', ')
+    return str;
+  }
+}
