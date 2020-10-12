f2e6a746bdb6a116ba1deb89863ed6ce03b3b6c9
Zachary Perkins <ezmac028@gmail.com>
2020-10-12 17:12:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
-function nowServing(katzDeliLine) {
-  if (katzDeliLine.length > 0) return `Currently serving $(katzDeliLine.shift()}.`;
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length > 0) return `Currently serving ${katzDeliLine.shift()}.`;
   else return "There is nobody waiting to be served!";
 }
